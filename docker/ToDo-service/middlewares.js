5fbc4e8c7684a69007877f1a6a217913d5c8177c
Ubuntu <azureuser@demo.3pd4o1uyfavelcx3ikmwkspgfe.bx.internal.cloudapp.net>
2024-05-12 18:00:00 +0000

    Added middlewares.js

--- docker/ToDo-service/middlewares.js
+++ docker/ToDo-service/middlewares.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const asyncHandler = fn => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
 
@@ -6,4 +8,25 @@ const errorHandler = (err, req, res, next) => {
   res.status(500).send({ error: 'Internal Server Error' });
 };
 
-module.exports = { asyncHandler, errorHandler };
+const authenticateJWT = (req, res, next) => {
+  const token = req.cookies.token;
+  console.log('Cookies:', req.cookies); // Log cookies
+  console.log('Token:', token); // Log token
+
+  if (!token) {
+    console.log('No token provided');
+    return res.status(401).send({ error: 'Unauthorized - No Token' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      console.log('Invalid token:', err);
+      return res.status(401).send({ error: 'Unauthorized - Invalid Token' });
+    }
+    req.user = user;
+    console.log('User authenticated:', user); // Log successful authentication
+    next(); // Pass control to the next middleware or route handler
+  });
+};
+
+module.exports = { asyncHandler, errorHandler, authenticateJWT };
