8123c6ba3ef6ae24cfd7bce55189b0babe4c8ddd
Ubuntu <azureuser@demo.3pd4o1uyfavelcx3ikmwkspgfe.bx.internal.cloudapp.net>
2024-05-12 12:00:00 +0000

    Added ToDo.js

--- docker/ToDo-service/models/ToDo.js
+++ docker/ToDo-service/models/ToDo.js
@@ -1,32 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const toDoSchema = new mongoose.Schema({
-//   text: {
-//     type: String,
-//     required: [true, 'Text for to-do is required'],
-//     minlength: 1,
-//     maxlength: 280 // Assuming a maximum length for the to-do text
-//   },
-//   completed: {
-//     type: Boolean,
-//     default: false
-//   }
-// }, {
-//   timestamps: true // Automatically adds createdAt and updatedAt fields
-// });
-
-// // Example of a custom static method
-// toDoSchema.statics.toggleCompleted = async function(id) {
-//   const todo = await this.findById(id);
-//   todo.completed = !todo.completed;
-//   return todo.save();
-// }
-
-// const ToDo = mongoose.model('ToDo', toDoSchema);
-
-// module.exports = ToDo;
-
-
 const mongoose = require('mongoose');
 
 const toDoSchema = new mongoose.Schema({
@@ -34,7 +5,7 @@ const toDoSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Text for to-do is required'],
     minlength: 1,
-    maxlength: 280 // Retaining your maximum length constraint
+    maxlength: 280
   },
   completed: {
     type: Boolean,
@@ -42,16 +13,20 @@ const toDoSchema = new mongoose.Schema({
   },
   comments: [{
     type: String
-  }], // Added an array of strings to hold comments
+  }],
   editing: {
     type: Boolean,
-    default: false // Field to determine if the ToDo is in editing mode
+    default: false
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   }
 }, {
-  timestamps: true // Retains the automatically added createdAt and updatedAt fields
+  timestamps: true
 });
 
-// Keeping your existing custom static method
 toDoSchema.statics.toggleCompleted = async function(id) {
   const todo = await this.findById(id);
   todo.completed = !todo.completed;
