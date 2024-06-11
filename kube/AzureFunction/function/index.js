75aeff877699e46d9844fbf4dd5db412ec5111d6
sabayneh1 <59586300+sabayneh1@users.noreply.github.com>
2024-06-11 18:22:12 -0400

    updated index.js of azure function

--- kube/AzureFunction/function/index.js
+++ kube/AzureFunction/function/index.js
@@ -1,6 +1,8 @@
 const azure = require('azure-storage');
 
 module.exports = async function (context, req) {
+    context.log('Received request:', req.body);
+
     const tableSvc = azure.createTableService(process.env.AzureWebJobsStorage);
     const tableName = "UserActivity";
 
@@ -13,6 +15,8 @@ module.exports = async function (context, req) {
             };
             context.done();
             return;
+        } else {
+            context.log("Table created or already exists: ", result);
         }
 
         const task = {
@@ -31,6 +35,7 @@ module.exports = async function (context, req) {
                     body: "Error logging user activity"
                 };
             } else {
+                context.log("Entity inserted: ", result);
                 context.res = {
                     status: 200,
                     body: "User activity logged successfully"
