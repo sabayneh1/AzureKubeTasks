63407e637237dabab96668b9b5753e822b913ea8
sabayneh1 <59586300+sabayneh1@users.noreply.github.com>
2024-06-11 19:31:03 -0400

    updated packge.json  of azure function

--- kube/AzureFunction/function/index.js
+++ kube/AzureFunction/function/index.js
@@ -1,46 +1,43 @@
 const azure = require('azure-storage');
 
 module.exports = async function (context, req) {
-    context.log('Received request:', req.body);
+    context.log('JavaScript HTTP trigger function processed a request.');
 
     const tableSvc = azure.createTableService(process.env.AzureWebJobsStorage);
     const tableName = "UserActivity";
 
-    tableSvc.createTableIfNotExists(tableName, function(error, result, response) {
+    tableSvc.createTableIfNotExists(tableName, function (error, result, response) {
         if (error) {
-            context.log.error("Error creating table: ", error);
+            context.log('Error creating table:', error);
             context.res = {
                 status: 500,
                 body: "Error creating table"
             };
             context.done();
             return;
-        } else {
-            context.log("Table created or already exists: ", result);
         }
 
         const task = {
-            PartitionKey: {'_': req.body.user},
-            RowKey: {'_': new Date().toISOString()},
-            Email: {'_': req.body.email},
-            Message: {'_': req.body.message},
-            Timestamp: {'_': new Date()}
+            PartitionKey: { '_': req.body.user },
+            RowKey: { '_': new Date().toISOString() },
+            Email: { '_': req.body.email },
+            Message: { '_': req.body.message },
+            Timestamp: { '_': new Date() }
         };
 
-        context.log("Inserting entity:", task);
+        context.log('Inserting entity:', task);
 
         tableSvc.insertEntity(tableName, task, function (error, result, response) {
-            if (error) {
-                context.log.error("Error inserting entity: ", error);
-                context.res = {
-                    status: 500,
-                    body: "Error logging user activity"
-                };
-            } else {
-                context.log("Entity inserted: ", result);
+            if (!error) {
                 context.res = {
                     status: 200,
                     body: "User activity logged successfully"
+                };
+            } else {
+                context.log('Error inserting entity:', error);
+                context.res = {
+                    status: 500,
+                    body: "Error logging user activity"
                 };
             }
             context.done();
